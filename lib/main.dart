c0b1d84fac50be3b9f3f59174e264b4f788e4a60
David Richter <david-richter-g@outlook.com>
2023-05-28 12:08:38 +0200

    Add sidebar with settings and theme switcher.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'dart:developer' as developer;
 import 'dart:convert';
+ import 'package:provider/provider.dart'; 
 
 import 'api.dart';
 
@@ -33,113 +34,181 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ChangeNotifierProvider<MyAppState>(
+      create: (_) => MyAppState(),
+
+      builder: (context, child) {
+          // No longer throws
+          return MaterialApp(
+            title: 'Namer App',
+            theme: ThemeData(
+              useMaterial3: true,
+              brightness: context.watch<MyAppState>().appTheme,
+            ),
+            home: MyHomePage(),
+          );
+        }
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  var appTheme = Brightness.light;
+
+  void setTheme(Brightness brightness) {
+    appTheme = brightness;
+
+    notifyListeners();
+  }
+}
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<Fonts>? fonts;
+  var selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    fonts = fetchFonts();
-    developer.log("Hello");
   }
-
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: (
-                Padding(
-                  padding: const EdgeInsets.only(top: 10,bottom: 10,left: 10,right: 10),
-                  child: FutureBuilder<Fonts>(
-                  future: fonts, 
-                  builder: ((context, snapshot) {
-                  if (snapshot.hasData) {
-                    return GridView.count(
-                      primary: false,
-                      padding: const EdgeInsets.all(20),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 3,
-                      children: snapshot.data!.items.map((font) => FontCard(font: font)).toList()
-                    );
-                  } 
-                  return const CircularProgressIndicator();
-                })),
-                )
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = FontPage();
+        break;
+      case 1:
+        page = SettingsPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var theme = Theme.of(context);
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home', style: theme.textTheme.bodyMedium),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.settings),
+                      label: Text('Settings', style: theme.textTheme.bodyMedium),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
               ),
-            )
-          ],
+              Expanded(
+                child: Container(
+                  color: theme.colorScheme.primaryContainer,
+                  child: page,
+                ), 
+            ),
+            ]
+          ),
+        );
+      }
+    );
+  }
+}
+
+class FontPage extends StatelessWidget {
+  Future<Fonts>? fonts = fetchFonts();
+
+  Widget build(BuildContext context) {
+    return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Expanded(
+        child: (
+          Padding(
+            padding: const EdgeInsets.only(top: 10,bottom: 10,left: 10,right: 10),
+            child: FutureBuilder<Fonts>(
+            future: fonts, 
+            builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                children: snapshot.data!.items.map((font) => FontCard(font: font)).toList()
+              );
+            } 
+            return const CircularProgressIndicator();
+          })),
+          )
         ),
-      ),
+      )
+    ],
+  );
+  }
+
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var theme = Theme.of(context);
+
+    return ListView(
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Settings',
+                  style: theme.textTheme.displayLarge,
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Row(children: [
+                  Text(
+                    'Dark Mode',
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (appState.appTheme == Brightness.light) {
+                        appState.setTheme(Brightness.dark);
+                      } else {
+                        appState.setTheme(Brightness.light);
+                      }
+                    },
+                    child: Text(
+                      'Switch',
+                      style: theme.textTheme.bodyMedium,
+                    ),
+                  ),
+                ]),
+              ],
+            ))
+      ],
     );
   }
 }
