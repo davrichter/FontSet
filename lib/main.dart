0b073021c650dbc160ffbdcb387fc52fc528bd8f
David Richter <david-richter-g@outlook.com>
2023-05-28 13:15:24 +0200

    Remove unused import.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 import 'dart:developer' as developer;
 import 'dart:convert';
  import 'package:provider/provider.dart'; 
