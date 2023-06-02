5897e836675497a77ccca894f3321c0977d6c814
David Richter <david-richter-g@outlook.com>
2023-06-02 19:00:03 +0200

    Add basic Linux and possibly macOS notifications.

--- lib/notification_sender.dart
+++ lib/notification_sender.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:win_toast/win_toast.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void sendNotification(String message) {
   if (Platform.isWindows) {
@@ -11,7 +12,7 @@ void sendNotification(String message) {
       clsid: 'your-notification-activator-guid-2EB1AE5198B7',
     );
 
-      var xml = """
+    var xml = """
       <toast launch='conversationId=9813'>
           <visual>
               <binding template='ToastGeneric'>
@@ -21,6 +22,33 @@ void sendNotification(String message) {
       </toast>
 
       """;
-      WinToast.instance().showCustomToast(xml: xml);
+    WinToast.instance().showCustomToast(xml: xml);
+  }
+
+  if (Platform.isLinux) {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    const LinuxInitializationSettings initializationSettingsLinux =
+        LinuxInitializationSettings(defaultActionName: 'Open notification');
+
+    const DarwinInitializationSettings initializationSettingsMacOS =
+        DarwinInitializationSettings();
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+            linux: initializationSettingsLinux,
+            macOS: initializationSettingsMacOS);
+
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    flutterLocalNotificationsPlugin.show(
+      123,
+      'FontSet',
+      message,
+      null,
+    );
   }
 }
